f182998bfdac0c73533036ffb305c58a5abe6c3b
Uthaeus <Uth@github.com>
2018-02-25 21:20:20 -0700

    interpolation 2

---
+++ interpolation.js
@@ -0,0 +1,12 @@
+
+const lyrics = 'Never gonna give you up';
+console.log("I'm " + lyrics);
+
+//using backtick for string interpolation
+
+console.log(`I'm ${lyrics}`);
+
+//interprets code inside brackets as pure javascript code.
+//Ex:
+
+console.log(`I'm ${2 + 2}`);
