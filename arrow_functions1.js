68aa72f2b8bd26ceef3ddcb3a0326e7dc34b4906
Uthaeus <Uth@github.com>
2018-02-25 21:37:09 -0700

    Introduction to arrow functions

---
+++ arrow_functions1.js
@@ -0,0 +1,24 @@
+
+// function fullName(fName, lName) {
+//   console.log(`${fName} ${lName}`);
+// }
+
+// fullName('Kristine', 'Hudgens');
+
+fullName = function(fName, lName) {
+  console.log(`${fName} ${lName}`);
+}
+
+fullName('Kristine', 'Hudgens');
+
+helloWorld = () => { console.log("Hi there"); }
+
+helloWorld();
+
+firstName = fName => { console.log(fName.toUpperCase()); }
+firstName('Kristine');
+
+fullName = (fName, lName) => { 
+  console.log(`${fName} ${lName}`);
+}
+fullName('Kristine', 'Hudgens');
