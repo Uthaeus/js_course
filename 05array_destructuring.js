de65770f24db5cf930b826525556916588cbe9d5
Uthaeus <romanlavery@gmail.com>
2018-03-07 09:24:31 -0700

    Review of array destructuring

--- 05array_destructuring.js
+++ 05array_destructuring.js
@@ -5,7 +5,9 @@ const apiList = [
   'https://api.github.com/users/jordanhudgens'
 ]
 
+// assigns elements in order to variables
 const [posts, repos, profile] = apiList;
-console.log(posts);
-console.log(repos);
-console.log(profiles);
+
+console.log(posts); // => 'https://api.dailysmarty.com/posts'
+console.log(repos); // => 'https://api.github.com/users/jordanhudgens/repos'
+console.log(profiles); // => 'https://api.github.com/users/jordanhudgens'
